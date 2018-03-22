a6fe8d560af98b23be7573667c28995b03e42f14
Siarhei Mahlysh <maglish@mail.ru>
2018-03-22 11:12:10 +0300

    Initial commit

---
+++ CsvParser.cs
@@ -0,0 +1,42 @@
+﻿namespace CsvParserApp
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Parses csv file
+    /// </summary>
+    public class CsvParser
+    {
+        /// <summary>
+        /// Contains columns names
+        /// </summary>
+        public List<string> Headers { get; private set; }
+
+        /// <summary>
+        /// Contains all rows without header
+        /// </summary>
+        public List<List<string>> Content { get; private set; }
+
+        /// <summary>
+        /// Parses csv located in specified path separated by commas by default
+        /// </summary>
+        /// <param name="path">file path including name</param>
+        /// <param name="delemiters">csv separators</param>
+        public void Parse(string path, char[] delemiters = null)
+        {
+            char[] separators = delemiters ?? new[] { ',' };
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            List<string> rows = File.ReadAllText(path).Split('\n').ToList();
+
+            this.Headers = rows.First().Split(separators).ToList();
+            this.Content = rows.Skip(1).Select(s => s.Split(separators).ToList()).ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CsvParserApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CsvParser csvParser = new CsvParser();
+            csvParser.Parse("EightKSearch2.csv");
+
+
+            foreach (var header in csvParser.Headers)
+            {
+                Console.WriteLine(header);
+            }
+
+            foreach (var row in csvParser.Content)
+            {
+                foreach (var cell in row)
+                {
+                    Console.WriteLine(cell);
+                }
+            }
+        }
+    }
+}
